d165691c9bed577c5db180c0677a6f7989575d0b
Souvik Khan <souvik97381@gmail.com>
2024-08-19 02:11:18 +0530

    added tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <>
     <Button appName="aptitude test" className={styles.secondary}>
           Open alert
     </Button>
+    <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    </>
   );
 }
