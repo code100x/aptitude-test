a8ebfb2a5d77c095449580f97c42b61a9ad40647
vanshavenger <vanshchopra101@gmail.com>
2024-08-25 16:35:43 +0530

    fixed errors (can be imporved)

--- src/app/(main)/(non-exam-section)/user-results/page.tsx
+++ src/app/(main)/(non-exam-section)/user-results/page.tsx
@@ -4,5 +4,5 @@ import UserResults  from '@/components/exams/user-results'
 export default async function UserResultsPage() {
   const results = await getUserResults()
 
-  return <UserResults results={results} />
+  return <UserResults />
 }
